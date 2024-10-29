3ad1748abb9b7c40bff19584265539e1ea349202
Trực Trần Trung <trantruc499@gmail.com>
2024-10-29 20:23:16 +0700

    Add: wwwroot-employee Add: Area-Controller(Dashboard, ManagerEmployee, ManagerCustomer, ManagerProduct, ManagerCategory, ManagerOrder)

---
+++ B3cBonsaiWeb/Areas/Employee/Controllers/EmployeeProfileController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace B3cBonsaiWeb.Areas.Employee.Controllers
+{
+    public class EmployeeProfileController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ B3cBonsaiWeb/Areas/Employee/Controllers/Staff/ManagerOrderController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace B3cBonsaiWeb.Areas.Employee.Controllers.Staff
+{
+    public class ManagerOrderController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult OrderSummary()
+        {
+            return View();
+        }
+    }
+}
