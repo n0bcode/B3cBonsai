49ffac67bf224cdc82155753c6d902756b6ccfaa
Trực Trần Trung <trantruc499@gmail.com>
2024-10-31 00:11:42 +0700

    Cập nhập: Chia ra view phương thức DetailWithDelete với Upsert trong /Employee/Manager(Employee/Customer/Product). Thêm folder images lưu hình ảnh và 2 folder customer/employee trong wwwroot/js/ để thực hiện các phương thức cho controller với mã js.

--- B3cBonsaiWeb/Areas/Employee/Controllers/Staff/ManagerProductController.cs
+++ B3cBonsaiWeb/Areas/Employee/Controllers/Staff/ManagerProductController.cs
@@ -8,5 +8,14 @@ namespace B3cBonsaiWeb.Areas.Employee.Controllers.Staff
         {
             return View();
         }
+
+        public IActionResult Upsert()
+        {
+            return View();
+        }
+        public IActionResult DetailWithDelete()
+        {
+            return View();
+        }
     }
 }
