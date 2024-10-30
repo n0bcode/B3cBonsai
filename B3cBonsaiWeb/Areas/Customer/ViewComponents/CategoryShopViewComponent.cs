25058a0013c0805476749544db1164a651f2cf5d
Trực Trần Trung <trantruc499@gmail.com>
2024-10-30 19:54:11 +0700

    Thay đổi: Thêm mã comment chú thích cho các /Customer/{Controller} Cắt mã Carousel trong /Customer/Home/Index thành hai view component lần lượt CommonProducts và PopularProducts; mã Carousel CategoryShop trong /Customer/Cart/Index thành view component - /Customer/Home/Index : Gồm các ViewComponent PopularProducts, CommonProducts. - /Customer/ClientProduct/Detail : Gồm ViewComponent PopularProducts - /Customer/Cart/Index : Gồm các ViewComponent PopularProducts, CategoryShop.

---
+++ B3cBonsaiWeb/Areas/Customer/ViewComponents/CategoryShopViewComponent.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace B3cBonsaiWeb.Areas.Customer.ViewComponents
+{
+    //Hiển thị danh sách sản phẩm trong /Customer/Cart/Index khi giỏ hàng trống
+    public class CategoryShopViewComponent : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            Random rd = new Random();
+            var listObjs = rd.NextDouble() < 0.5 ? new List<Object>() : null;
+            return View(listObjs);
+        }
+    }
+}
---
+++ B3cBonsaiWeb/Areas/Customer/ViewComponents/CommonProductsViewComponent.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace B3cBonsaiWeb.Areas.Customer.ViewComponents
+{
+    //Hiển thị danh sách sản phẩm trong /Customer/Home/Index
+    //Hiển thị danh sách 1 số sản phẩm ngẫu nhiên - 9 sản phẩm hiển thị
+    public class CommonProductsViewComponent : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            Random rd = new Random();
+            var listObjs = rd.NextDouble() < 0.5 ? new List<Object>() : null;
+            return View(listObjs);
+        }
+    }
+}
