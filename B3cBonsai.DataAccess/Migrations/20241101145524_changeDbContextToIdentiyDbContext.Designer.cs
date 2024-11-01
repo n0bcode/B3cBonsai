718c5dbd4a33f332da18ca497847a4d7e366b9de
Trực Trần Trung <trantruc499@gmail.com>
2024-11-01 22:19:31 +0700

    Cập nhập: Sửa kế thừa từ DbContext sang IdentityDbContext<IdentityUser>

--- B3cBonsai.DataAccess/Migrations/20241101145524_changeDbContextToIdentiyDbContext.Designer.cs
+++ B3cBonsai.DataAccess/Migrations/20241101145524_changeDbContextToIdentiyDbContext.Designer.cs
@@ -4,6 +4,7 @@ using B3cBonsai.DataAccess.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace B3cBonsai.DataAccess.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20241101145524_changeDbContextToIdentiyDbContext")]
+    partial class changeDbContextToIdentiyDbContext
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
