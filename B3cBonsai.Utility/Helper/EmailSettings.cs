6df588a36300a57aa8e82a9f40ae001020a7794d
Trực Trần Trung <trantruc499@gmail.com>
2024-11-13 12:43:41 +0700

    Update: Feture SendEmail/Source from AWS

---
+++ B3cBonsai.Utility/Helper/EmailSettings.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace B3cBonsai.Utility.Helper
+{
+    public class EmailSettings
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string Host { get; set; }
+        public string Displayname { get; set; }
+        public int Port { get; set; }
+        public string SmtpServer { get; set; }
+        public string Username { get; set; }
+    }
+}
