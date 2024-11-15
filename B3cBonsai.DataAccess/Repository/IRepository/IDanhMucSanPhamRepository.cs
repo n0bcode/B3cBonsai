91b6d74aa94161bc1bf8ccdbc0565292d8337e3f
Trung <Trung@DESKTOP-FQQS4ED>
2024-11-15 09:23:59 +0700

    Update: Add method Repo DanhMucSanPham, CRUD Category

--- B3cBonsai.DataAccess/Repository/IRepository/IDanhMucSanPhamRepository.cs
+++ B3cBonsai.DataAccess/Repository/IRepository/IDanhMucSanPhamRepository.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,5 +11,8 @@ namespace B3cBonsai.DataAccess.Repository.IRepository
     public interface IDanhMucSanPhamRepository : IRepository<DanhMucSanPham>
     {
         public void Update(DanhMucSanPham obj);
+
+        // Thêm phương thức GetFirstOrDefault để lấy một bản ghi theo điều kiện
+        DanhMucSanPham GetFirstOrDefault(Expression<Func<DanhMucSanPham, bool>> filter);
     }
 }
