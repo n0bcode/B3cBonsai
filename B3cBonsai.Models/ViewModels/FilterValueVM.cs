11c0bf847a0cd1e7679f082643838fb1fe18899a
Trực Trần Trung <trantruc499@gmail.com>
2024-11-16 19:49:11 +0700

    Update: Paged & Search in /Customer/ClientProduct

---
+++ B3cBonsai.Models/ViewModels/FilterValueVM.cs
@@ -0,0 +1,12 @@
+﻿namespace B3cBonsai.Models.ViewModels
+{
+    public class FilterValueVM
+    {
+
+        public List<DanhMucSanPham> Categories { get; set; }
+        public int MinimumPrice { get; set; }
+        public int MaximumPrice { get; set; }
+        public int InStock { get; set; }
+        public int OutOfStock { get; set; }
+    }
+}
---
+++ B3cBonsaiWeb/wwwroot/js/customer/clientProduct.js
@@ -0,0 +1,48 @@
+﻿
+const loadGridViewProduct = () => {
+    $.ajax({
+        url: '/customer/clientproduct/ListPagedProduct',
+        method: 'GET',
+        data: {},
+        success: function (data) {
+            $('#gridViewListProduct').html(data)
+        },
+        error: (xhr) => {
+            console.log(xhr)
+        }
+    });
+}
+$(document).ready(function () {
+    loadGridViewProduct();
+    $(document).on('click', '.pagination-container .pagination-page-box a', function (event) {
+        event.preventDefault();
+        $.ajax({
+            url: $(this).attr('href'),
+            method: 'GET',
+            success: (data) => {
+                $('#gridViewListProduct').html(data);
+            },
+            error: (xhr) => {
+                createAlert({ title: "Thông báo", content: "Lỗi load dữ liệu, vui lòng thử lại sau", type: 'warning', timeout: 5000 });
+            }
+        });
+    });
+
+    $(document).on('submit', '#filterProductForm', function (event) {
+        event.preventDefault();
+
+        // Lấy dữ liệu từ form
+        var formData = $(this).serialize();
+        $.ajax({
+            url: `/customer/clientproduct/ListPagedProduct`,
+            method: "GET",
+            data: formData,
+            success: (data) => {
+                $('#gridViewListProduct').html(data)
+            },
+            error: (xhr) => {
+                console.log(xhr)
+            }
+        });
+    });
+});
