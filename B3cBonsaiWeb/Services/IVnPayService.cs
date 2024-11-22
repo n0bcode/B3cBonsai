f2bf8e5cc6d00b0d1e82c384c0135f33f72c2188
Trung <Trung@DESKTOP-FQQS4ED>
2024-11-22 11:22:15 +0700

    Update: /Customer/Payment + VnPay + Chat Bot Telegram

---
+++ B3cBonsaiWeb/Services/IVnPayService.cs
@@ -0,0 +1,11 @@
+﻿using B3cBonsai.Models.ViewModels;
+
+namespace B3cBonsaiWeb.Services
+{
+    public interface IVnPayService
+    {
+
+        string CreatePaymentUrl(HttpContext context, VnPaymentRequestModel model);
+        VnPaymentResponseModel PaymentExecute(IQueryCollection collections);
+    }
+}
