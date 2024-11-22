65477f46e7b19353ec8886e76a74271dcf8ed141
Trung <Trung@DESKTOP-FQQS4ED>
2024-11-22 11:22:15 +0700

    Update: /Customer/Payment + VnPay + Chat Bot Telegram

---
+++ B3cBonsaiWeb/Areas/Customer/Controllers/CheckoutController.cs
@@ -0,0 +1,44 @@
+﻿using B3cBonsai.Utility;
+using Microsoft.AspNetCore.Mvc;
+using B3cBonsai.DataAccess.Repository.IRepository;
+using B3cBonsaiWeb.Services;
+
+namespace B3cBonsaiWeb.Areas.Customer.Controllers
+{
+    [Area("Customer")]
+    public class CheckoutController : Controller
+    {
+        private readonly IVnPayService _vnPayService;
+        private readonly IUnitOfWork _unitOfWork;
+
+        public CheckoutController(IVnPayService vnPayService, IUnitOfWork unitOfWork)
+        {
+            _vnPayService = vnPayService;
+            _unitOfWork = unitOfWork;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> PaymentCallBack()
+        {
+            var vnpayData = HttpContext.Request.Query;
+
+            var response = _vnPayService.PaymentExecute(vnpayData);
+
+            if (response.Success)
+            {
+                var order = await _unitOfWork.DonHang.Get(o => o.Id == int.Parse(response.OrderId));
+                if (order != null)
+                {
+                    order.TrangThaiThanhToan = SD.PaymentStatusApproved;
+                    order.NgayThanhToan = DateTime.Now;
+                    _unitOfWork.DonHang.Update(order);
+                    _unitOfWork.Save();
+                }
+
+                return View("PaymentSuccess", response);
+            }
+
+            return View("PaymentFailed", response);
+        }
+    }
+}
