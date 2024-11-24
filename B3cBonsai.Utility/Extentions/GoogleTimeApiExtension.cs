031110657a71cfe7585e920d150f717aa3c752aa
Trực Trần Trung <trantruc499@gmail.com>
2024-11-24 18:23:16 +0700

    Cập nhập: Giao diện sau khi thanh toán bằng thẻ, tách chức năng bình luận ra thêm 1 CommentController với comment.js

---
+++ B3cBonsai.Utility/Extentions/GoogleTimeApiExtension.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace B3cBonsai.Utility.Extentions
+{
+    public class GoogleTimeApiExtension
+    {
+        private static readonly HttpClient _httpClient = new HttpClient();
+
+        public static async Task<DateTimeOffset> GetCurrentTimeAsync()
+        {
+            var response = await _httpClient.GetAsync("https://www.googleapis.com/oauth2/v1/certs");
+            response.EnsureSuccessStatusCode();
+
+            // Lấy thời gian hiện tại từ Google API
+            var currentTimestamp = DateTime.Parse(response.Headers.GetValues("Date").First());
+            return currentTimestamp;
+        }
+    }
+}
