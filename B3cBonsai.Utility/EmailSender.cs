9e5f75b17831ba95ed834d4710a071fc7d852cdf
Trực Trần Trung <trantruc499@gmail.com>
2024-11-27 08:16:09 +0700

    Cập nhập: Thêm khung nền tin nhắn Email

--- B3cBonsai.Utility/EmailSender.cs
+++ B3cBonsai.Utility/EmailSender.cs
@@ -19,49 +19,34 @@ namespace B3cBonsai.Utility
         {
             this.emailSettings = options.Value;
         }
-
         public async Task SendEmailAsync(string toEmail, string subject, string message)
         {
-
-            /*var apiKey = Environment.GetEnvironmentVariable("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");
-            var client = new SendGridClient(apiKey);
-            var from = new EmailAddress("test@example.com", "Example User");
-            var to = new EmailAddress(toEmail, "Example User");
-            var plainTextContent = "and easy to do anywhere, even with C#";
-            var htmlContent = message;
-            var msg = MailHelper.CreateSingleEmail(from, to, subject, plainTextContent, htmlContent);
-            var response = await client.SendEmailAsync(msg);*/
-
-            #region//Past
             var email = new MimeMessage();
-            email.Sender = MailboxAddress.Parse(toEmail);
+            email.Sender = MailboxAddress.Parse(emailSettings.Email);
             email.To.Add(MailboxAddress.Parse(toEmail));
             email.Subject = subject;
-            var builder = new BodyBuilder();
 
+            // Đọc nội dung tệp HTML
+            var templatePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "mail", "template.html");
+            var htmlTemplate = await File.ReadAllTextAsync(templatePath);
 
-            byte[] fileBytes;
-            if (File.Exists("Attachment/dummy.pdf"))
-            {
-                FileStream file = new FileStream("Attachment/dummy.pdf", FileMode.Open, FileAccess.Read);
-                using (var ms = new MemoryStream())
-                {
-                    file.CopyTo(ms);
-                    fileBytes = ms.ToArray();
-                }
-                builder.Attachments.Add("attachment.pdf", fileBytes, ContentType.Parse("application/octet-stream"));
-                builder.Attachments.Add("attachment2.pdf", fileBytes, ContentType.Parse("application/octet-stream"));
-            }
+            // Thay thế các placeholder với dữ liệu thật
+            var htmlBody = htmlTemplate
+                .Replace("{{title}}", subject)
+                .Replace("{{time}}", DateTime.UtcNow.ToString("dd/MM/yyyy"))
+                .Replace("{{name}}", "B3cBonsai")
+                .Replace("{{email}}", toEmail)
+                .Replace("{{message}}", message);
 
-            builder.HtmlBody = @"<div style=""width: 100%; background-color: blue;"">" + message + @"</div>";
-            email.Body = builder.ToMessageBody();
+            email.Body = new TextPart("html") { Text = htmlBody };
 
             using var smtp = new SmtpClient();
             smtp.Connect(emailSettings.Host, emailSettings.Port, SecureSocketOptions.StartTls);
             smtp.Authenticate(emailSettings.Email, emailSettings.Password);
             await smtp.SendAsync(email);
             smtp.Disconnect(true);
-            #endregion
         }
+
+
     }
 }
