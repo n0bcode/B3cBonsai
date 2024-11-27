50baf5703f8c32aa8410183e752b7dfcca4ec100
Trực Trần Trung <trantruc499@gmail.com>
2024-11-27 11:12:08 +0700

    Chỉnh sửa: Sửa nội dung breadcrumb. Đổi https://B3cBonsai.dexignzone.com/ thành '-'

--- B3cBonsaiWeb/wwwroot/employee/vendor/dropzone/dist/dropzone.js
+++ B3cBonsaiWeb/wwwroot/employee/vendor/dropzone/dist/dropzone.js
@@ -1829,11 +1829,11 @@ function (_Emitter) {
                       return;
                     }
 
-                    file.fullPath = "".concat(path, "https://B3cBonsai.dexignzone.com/").concat(file.name);
+                    file.fullPath = "".concat(path, "_").concat(file.name);
                     return _this6.addFile(file);
                   });
                 } else if (entry.isDirectory) {
-                  _this6._addFilesFromDirectory(entry, "".concat(path, "https://B3cBonsai.dexignzone.com/").concat(entry.name));
+                  _this6._addFilesFromDirectory(entry, "".concat(path, "_").concat(entry.name));
                 }
               } // Recursively call readEntries() again, since browser only handle
               // the first 100 entries.
