5d50eced718fb3aee0f3ace49f2f892f2e347874
Trực Trần Trung <trantruc499@gmail.com>
2024-11-30 11:21:03 +0700

    Thêm RichText cho việc cập nhập mô tả sản phẩm

--- B3cBonsaiWeb/wwwroot/js/employee/managerProduct.js
+++ B3cBonsaiWeb/wwwroot/js/employee/managerProduct.js
@@ -103,6 +103,33 @@ $(document).ready(function () {
     $('[data-bs-toggle="offcanvas"]').on('click', function () {
         myOffcanvas.show();
     });
+
+
+    $('#upsertObject').on('shown.bs.offcanvas', function () {
+        tinymce.init({
+            selector: 'textarea.tinymce',
+            plugins: [
+                // Core editing features
+                'anchor', 'autolink', 'charmap', 'codesample', 'emoticons', 'image', 'lists', 'table', 'visualblocks', 'wordcount',
+                // Your account includes a free trial of TinyMCE premium features
+                // Try the most popular premium features until Nov 7, 2024:
+                'checklist', 'mediaembed', 'casechange', 'export', 'formatpainter', 'pageembed', 'a11ychecker', 'tinymcespellchecker', 'permanentpen', 'powerpaste', 'advtable', 'advcode', 'editimage', 'advtemplate', 'ai', 'mentions', 'tinycomments', 'tableofcontents', 'footnotes', 'mergetags', 'autocorrect', 'typography', 'inlinecss', 'markdown',
+                // Early access to document converters
+                'importword', 'exportword', 'exportpdf'
+            ],
+            toolbar: 'undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | align lineheight | checklist numlist bullist indent outdent',
+            tinycomments_mode: 'embedded',
+            tinycomments_author: 'Author name',
+            mergetags_list: [
+                { value: 'First.Name', title: 'First Name' },
+                { value: 'Email', title: 'Email' },
+            ],
+            ai_request: (request, respondWith) => respondWith.string(() => Promise.reject('See docs to implement AI Assistant')),
+            exportpdf_converter_options: { 'format': 'Letter', 'margin_top': '1in', 'margin_right': '1in', 'margin_bottom': '1in', 'margin_left': '1in' },
+            exportword_converter_options: { 'document': { 'size': 'Letter' } },
+            importword_converter_options: { 'formatting': { 'styles': 'inline', 'resets': 'inline', 'defaults': 'inline', } },
+        });
+    });
 });
 
 const loadDetailWithDelete = (id) => {
